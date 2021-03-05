92cc31b4da048568cb673e001cee6c9200b934a7
frodrisu <frodrisu@everis.com>
2021-03-04 20:19:32 -0500

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Parcial';
+  isHome = true;
+  isServices = false;
+  isAbout = false;
+  isContact = false;
+  isFAQ = false;
+
+
+  home() {
+    this.isHome = true;
+    this.isServices = false;
+    this.isAbout = false;
+    this.isContact = false;
+    this.isFAQ = false;
+  }
+  services() {
+    this.isHome = false;
+    this.isServices = true;
+    this.isAbout = false;
+    this.isContact = false;
+    this.isFAQ = false;
+  }
+  about() {
+    this.isHome = false;
+    this.isServices = false;
+    this.isAbout = true;
+    this.isContact = false;
+    this.isFAQ = false;
+  }
+  contact() {
+    this.isHome = false;
+    this.isServices = false;
+    this.isAbout = false;
+    this.isContact = true;
+    this.isFAQ = false;
+  }
+  FAQ() {
+    this.isHome = false;
+    this.isServices = false;
+    this.isAbout = false;
+    this.isContact = false;
+    this.isFAQ = true;
+  }
 }
